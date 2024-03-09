d3e31f2be9577c37897f2bd9bfec2318da6cfe9e
KnarikSoghoyan <knariksoghoyan@gmail.com>
2024-03-09 19:24:27 +0400

    Adding product schema

--- src/routes/product-router.js
+++ src/routes/product-router.js
@@ -1,4 +1,8 @@
 import { Router } from 'express';
 const router = Router();
 
+import { getProducts } from '../controllers/product-controller.js';
+
+router.get('/', getProducts);
+
 export default router;
