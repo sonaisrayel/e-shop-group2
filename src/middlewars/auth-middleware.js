88a0d29ab8bc1e0c5ceca846526b94ef8e824567
Narine <narartmar@gmail.com>
2024-03-14 11:06:10 +0400

    Added authorization middleware

---
+++ src/middlewars/auth-middleware.js
@@ -0,0 +1,13 @@
+import JWTLib from "../libs/jwt-lib.js";
+
+export default class Authorize {
+    static async authorized (req, res, next){
+        try {
+            const { authorization } = req.headers;
+            req.userInfo = await JWTLib.verifyUserToken(authorization);
+            next();
+        } catch (e) {
+            return next(e.message)
+        }
+    }
+}
