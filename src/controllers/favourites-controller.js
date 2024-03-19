4dadaf2a5435adfa0d3a2737f5c04d4017761645
KnarikSoghoyan <knariksoghoyan@gmail.com>
2024-03-20 03:47:18 +0400

    Add getFavorites function

--- src/controllers/favourites-controller.js
+++ src/controllers/favourites-controller.js
@@ -15,3 +15,19 @@ export const createFavourite = async (req, res) => {
     }
 }
 
+export const getFavourites = async (req, res) => {
+    try {
+        const {limit, skip} = req.query
+        const { userInfo } = req;
+        
+        const favourites = await Favourites.find({userId:userInfo.id}).limit(limit).skip(skip);
+      
+        if (!favourites.length) {
+            throw new Error('Favourites not found!');            
+        }
+        res.status(200).send({favourites});
+
+    } catch (error) {
+        res.status(404).send({ "message": error.message });
+    }
+}
