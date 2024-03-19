803953efba7d133384540b22dbb5423cf8e76f6f
KnarikSoghoyan <knariksoghoyan@gmail.com>
2024-03-20 01:58:06 +0400

    Add username in JWT token

--- src/libs/jwt-lib.js
+++ src/libs/jwt-lib.js
@@ -2,8 +2,8 @@ import JWT from 'jsonwebtoken';
 const { SECRET } = process.env;
 
 const signUserToken = async (payload) => {
-    const { id, email, role } = payload;
-    return JWT.sign({ id, email, role }, SECRET, {expiresIn: '15d'});
+    const { id, email, username, role } = payload;
+    return JWT.sign({ id, email, username, role }, SECRET, {expiresIn: '15d'});
 };
 
 const verifyUserToken = async (token) => {
