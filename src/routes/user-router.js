3b3356e5b668d3bbecc59a28d9de3117b26dd45b
sonaisrayel <sonai@brightcom.com>
2024-03-23 22:09:27 +0400

    add changed to user upload

--- src/routes/user-router.js
+++ src/routes/user-router.js
@@ -1,18 +1,19 @@
 import { Router } from "express";
 const router = Router();
+import { upload } from '../libs/multer-lib.js';
 
 
 import {
   getUsers,
   getUserProducts,
   updateUser,
-  addUserImage
+  addUserImage,
 } from "../controllers/user-controller.js";
 import Authorize from "../middlewars/auth-middleware.js";
 
 router.get("/", Authorize.isAdmin, getUsers);
 router.get("/:id/products", getUserProducts);
 router.patch("/", Authorize.authorized, updateUser);
-router.post('/image', Authorize.authorized, addUserImage);
+router.patch("/image", upload.single('file'), Authorize.authorized,addUserImage);
 
 export default router;
