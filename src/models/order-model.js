5ac7abd19289e56c9eb76e8919f96f8a41af7471
Gog <gogtepoyan@gmail.com>
2024-04-03 18:24:00 +0400

    Fix_orders_V.0.2

---
+++ src/models/order-model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  items: [
+    {
+      productId: {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+});
+
+const Order = mongoose.model("Order", orderSchema);
+export { Order };
--- src/routes/order-router.js
+++ src/routes/order-router.js
@@ -1,4 +1,18 @@
 import { Router } from "express";
 const router = Router();
 
+import {
+  createOrderFromBucket,
+  getOrders,
+  getUserOrders,
+  getUserOrder,
+  } from "../controllers/order-controller.js";
+
+router.post("/", createOrderFromBucket);
+router.get("/user", getUserOrders);
+router.get("/:id", getUserOrder);
+// router.get("/owner", getOwnerOrders);
+router.get("/", getOrders);
+// router.delete("/:id", deleteOrder);
+
 export default router;
