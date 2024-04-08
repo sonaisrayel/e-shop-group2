872ddc45c52d68a4bc96403d8e6b50dca1dbb611
Narine <narartmar@gmail.com>
2024-04-08 13:48:55 +0400

    Fixed admin login

--- src/controllers/admin-controller.js
+++ src/controllers/admin-controller.js
@@ -1,23 +1,23 @@
 import jwtLib from "../libs/jwt-lib.js";
 import ResponseHandler from "../handlers/response-handling.js";
 import { validationError } from "../handlers/error-handling.js";
+const { ADMIN_USERNAME, ADMIN_PASSWORD } = process.env 
 
 export const adminLogin = async (req, res, next) => {
   try {
-    const adminUsername = process.env.ADMIN_USERNAME;
-    const adminPassword = process.env.ADMIN_PASSWORD;
+    
 
     const { username, password } = req.body;
 
-    if (username !== adminUsername && password !== adminPassword) {
+    if (password !== ADMIN_PASSWORD || username !== ADMIN_USERNAME) {
       return validationError(res, "Access denied! You are not admin.");
     }
     const token = await jwtLib.signAdminToken({
       role: "admin",
-      username: adminUsername,
+      username: ADMIN_USERNAME,
     });
     return ResponseHandler.handlePostResponse(res, {
-      admin: { username: adminUsername },
+      admin: { username: ADMIN_USERNAME },
       token,
     });
   } catch (e) {
